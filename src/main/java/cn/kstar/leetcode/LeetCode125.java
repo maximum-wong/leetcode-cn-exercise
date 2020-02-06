57302e1841079b05142b3d010b9da27efc867395
maximum-wong <maximum2012@qq.com>
2020-02-06 16:52:37 +0800

    Added a new solution for LeetCode125

---
+++ src/main/java/cn/kstar/leetcode/LeetCode125.java
@@ -0,0 +1,49 @@
+package cn.kstar.leetcode;
+
+/**
+ * <h6>验证回文串</h6>
+ * 
+ * <p>给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+ * <br/><b>说明：</b>本题中，我们将空字符串定义为有效的回文串。</p>
+ * <pre><b>Examples: </b>
+ * Input: "A man, a plan, a canal: Panama"; Output: true
+ * 
+ * Input: "race a car"; Output: false
+ * </pre>
+ */
+public class LeetCode125 {
+
+	/**
+	 * <h6>双指针法</6>
+	 * 
+	 * @param  s
+	 * @return boolean
+	 */
+	public boolean isPalindrome(String s) {
+		if (s == null) {
+			return false;
+		}
+		// 空字符串是回文串
+		if (s.length() == 0) {
+			return true;
+		}
+		int left = 0;
+		int right = s.length() - 1;
+		while (left < right) {
+			// 左右指针所指向的字符不是字母和数字，做相应的移动处理
+			while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+				left++;
+			}
+			while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+				right--;
+			}
+			// 左右指针分别指向的字符不相等，不是回文串
+			if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+				return false;
+			}
+			left++;
+			right--;
+		}
+		return true;
+	}
+}
--- src/main/java/cn/kstar/leetcode/LeetCode135.java
+++ src/main/java/cn/kstar/leetcode/LeetCode135.java
@@ -7,10 +7,10 @@ package cn.kstar.leetcode;
  * <br/>你需要按照以下要求，帮助老师给这些孩子分发糖果：每个孩子至少分配到 1 个糖果。
  * <br/>相邻的孩子中，评分高的孩子必须获得更多的糖果。那么这样下来，老师至少需要准备多少颗糖果呢？</p>
  * <pre><b>Examples: </b>
- * Input: head = [1,0,2]; Output: 5
+ * Input: [1,0,2]; Output: 5
  * Explanation: 你可以分别给这三个孩子分发 2、1、2 颗糖果。
  * 
- * Input: head = [1,2,2]; Output: 4
+ * Input: [1,2,2]; Output: 4
  * Explanation: 你可以分别给这三个孩子分发 1、2、1 颗糖果。第三个孩子只得到 1 颗糖果，这已满足上述两个条件。
  * </pre>
  */
