5d178d2982576a836669bed032bae9e30c12919e
maximum-wong <maximum2012@qq.com>
2020-02-10 20:34:55 +0800

    Add a new solution for LeetCode25

--- src/main/java/cn/kstar/leetcode/LeetCode92.java
+++ src/main/java/cn/kstar/leetcode/LeetCode92.java
@@ -18,8 +18,8 @@ public class LeetCode92 {
      * <p><b>解题思路：</b>
      * <br/>添加虚拟头节点，并找到带反转的区间，记录前驱节点、后驱节点、开始节点和结束节点；
      * <br/>然后在反转区间内进行反转操作，获取操作后的子链表，前驱节点与子链表的头节点相连，
-     * <br/>子链表的尾节点与后驱节点相连；最后返回虚拟机头节点指向的next的链表。</p>
-     * 
+     * <br/>子链表的尾节点与后驱节点相连；最后返回虚拟头节点指向的next的链表。</p>
+     *
      * @param  head
      * @param  m
      * @param  n
@@ -57,7 +57,7 @@ public class LeetCode92 {
 
         // 反转区间最后一个节点与原链断开连接
         end.next = null;
-        // 前驱节点与反转后的链表头节点
+        // 前驱节点与反转后的链表头节点相连
         predecessor.next = reverseList(start);
         // 反转后开始节点变成尾节点与后继节点相连
         start.next = successor;
