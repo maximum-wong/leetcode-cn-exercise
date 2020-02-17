52c3c2aa05075481a42e4e78c117bd713f60aea0
maximum-wong <maximum2012@qq.com>
2020-02-17 15:03:53 +0800

    Added 2 solutions for LeetCode160

--- src/main/java/cn/kstar/leetcode/LeetCode237.java
+++ src/main/java/cn/kstar/leetcode/LeetCode237.java
@@ -4,7 +4,7 @@ import cn.kstar.leetcode.data_structure.ListNode;
 
 /**
  * <h6>删除链表中的节点
- * <br/></h6>
+ * <br/>Delete Node in a Linked List</h6>
  *
  * <p>请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
  * <br/>现有一个链表 -- head = [4,5,1,9]，它可以表示为： 4 -> 5 -> 1 -> 9
