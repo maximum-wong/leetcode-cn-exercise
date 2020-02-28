ef5d96dccce5d52fc4181ebd2dad8f4adca8e320
maximum-wong <maximum2012@qq.com>
2020-02-28 21:18:42 +0800

    Added a new solution for LeetCode1

--- src/main/java/cn/kstar/leetcode/LeetCode1.java
+++ src/main/java/cn/kstar/leetcode/LeetCode1.java
@@ -1,15 +1,19 @@
 package cn.kstar.leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * <h3>Two Sum
- * <br/>两数之和</h3>
+ * <h3>两数之和
+ * <br/>Two Sum</h3>
  * 
- * <p>Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * <p>给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+ * <br/>你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+ * <br/>Given an array of integers, return indices of the two numbers such 
+ * that they add up to a specific target. 
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
- * <br/>给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。</p>
+ * </p>
  * 
  * <pre><b>Example:</b>
  * Given nums = [2, 7, 11, 15], target = 9,
@@ -23,11 +27,11 @@ public class LeetCode1 {
      * <h6>暴力破解法<h6>
      * 
      * <p>时间复杂度：O(n^2)；空间复杂度：O(1)</p>
-     * 
      * @param  array
      * @param  target
      * @return int[]
      */
+    @Deprecated
     public int[] twoSum(int[] array, int target) {
         int[] ret = new int[2];
         for (int i = 0, len1 = array.length - 1; i < len1; i++) {
@@ -50,7 +54,6 @@ public class LeetCode1 {
      * <p>每次遍历时，在哈希表中保存数组的值（key）和索引（value），
      * <br/>算出目标值与当前数组值的差值，然后在哈希表中查找该差值，
      * <br/>若查找到，返回当前数组的索引和在哈希表中保存的该差值的所对应的索引值</p>
-     * 
      * @param  array
      * @param  target
      * @return int[]
@@ -74,7 +77,6 @@ public class LeetCode1 {
      * <h6>查表法（循环赋值）</h6>
      * 
      * <p>时间复杂度：O(n)；空间复杂度：O(n)</p>
-     * 
      * @param  array
      * @param  target
      * @return int[]
@@ -98,4 +100,37 @@ public class LeetCode1 {
         return ret;
     }
 
+    /**
+     * <h6>排序+双指针法</h6>
+     *
+     * <p>时间复杂度：O(nlgn + n)；空间复杂度：O(1)</p>
+     * @param array
+     * @param target
+     * @return
+     */
+    public int[] twoSum4(int[] array, int target) {
+        if (array == null || array.length == 0) {
+            return array;
+        }
+        
+        // 对数组进行排序
+        Arrays.sort(array);
+        
+        int[] ret = new int[2];
+        int left = 0;
+        int right = array.length-1;
+        while (left<right) {
+            if (array[left]+array[right]==target) {
+                ret[0] = array[left];
+                ret[1] = array[right];
+                return ret;
+            } else if (array[left]+array[right]<target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return ret;
+    }
 }
