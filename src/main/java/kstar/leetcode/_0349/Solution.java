ed0568409b452af810b3f1343fae761d5221a25a
maximum-wong <maximum2012@qq.com>
2020-06-15 23:19:40 +0800

    Added 3 solutions for leetcode 349

--- src/main/java/kstar/leetcode/_0349/Solution.java
+++ src/main/java/kstar/leetcode/_0349/Solution.java
@@ -3,6 +3,9 @@ package kstar.leetcode._0349;
 import java.util.Arrays;
 import java.util.HashSet;
 
+/**
+ * <h6>查表法</h6>
+ */
 public class Solution {
 
     public int[] intersection(int[] nums1, int[] nums2) {
@@ -11,11 +14,13 @@ public class Solution {
             return new int[0];
         }
 
+        // nums1放入set中去重
         HashSet<Integer> set1 = new HashSet<>(nums1.length);
         for (int num : nums1) {
             set1.add(num);
         }
 
+        // nums2放入set中去重
         HashSet<Integer> set2 = new HashSet<>(nums1.length);
         for (int num : nums2) {
             set2.add(num);
@@ -27,6 +32,13 @@ public class Solution {
         return getIntersection(smallSet, bigSet);
     }
 
+    /**
+     * <h6>遍历较小的set，判断较小set中的元素是否是跟较大的set中元素相同，求交集</h6>
+     * 
+     * @param set1
+     * @param set2
+     * @return
+     */
     private int[] getIntersection(HashSet<Integer> set1, HashSet<Integer> set2) {
         int[] output = new int[set1.size()];
         int idx = 0;
---
+++ src/main/java/kstar/leetcode/_0349/Solution4.java
@@ -0,0 +1,54 @@
+package kstar.leetcode._0349;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * <h6>排序+二分法</h6>
+ * <p>把一个数组进行排序，然后遍历另一个数组，对第一个数组进行二分查找，收集二分查找到的元素</p>
+ */
+public class Solution4 {
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        // 对一个数组进行排序
+        Arrays.sort(nums2);
+        for (int target : nums1) {
+            // 通过二分查找，判断该元素是否存在，收集存在的元素
+            if (binarySearch(nums2, target)) {
+                set.add(target);
+            }
+        }
+        int idx = 0;
+        int[] res = new int[set.size()];
+        for (int i : set) {
+            res[idx++] = i;
+        }
+        return res;
+    }
+
+    /**
+     * <h6>二分查找</h6>
+     * 
+     * @param nums
+     * @param target
+     * @return
+     */
+    private boolean binarySearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) >> 1;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+        }
+
+        return false;
+    }
+}
