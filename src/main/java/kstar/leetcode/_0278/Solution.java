1ba261d9e8579f9a9079d9b39e2c23f013ec87b9
maximum-wong <maximum2012@qq.com>
2020-06-18 21:20:33 +0800

    Added a new solution for leetcode#277

---
+++ src/main/java/kstar/leetcode/_0278/Solution.java
@@ -0,0 +1,34 @@
+package kstar.leetcode._0278;
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+boolean isBadVersion(int version); */
+class VersionControl {
+
+    private boolean[] versions = new boolean[1024];
+
+    boolean isBadVersion(int version) {
+        return versions[version - 1];
+    }
+}
+
+/**
+ * <h6>二分法</h6>
+ */
+public class Solution extends VersionControl {
+
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) >> 1;
+            // 是坏的版本，坏的一定在mid及mid左边
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else { // 不是坏的版本，坏的在mid的右边
+                left = mid + 1;
+            }
+        }
+        // 返回left，一定是第一个错误版本
+        return left;
+    }
+}
