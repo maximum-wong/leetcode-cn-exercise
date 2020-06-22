4b68aad22fee948cbc99fc22987beea1e15a3fde
maximum2012 <maximum2012@qq.com>
2020-06-22 16:20:30 +0800

    Added a 2 new solutions for leetcode#217

---
+++ src/main/java/kstar/leetcode/_0217/Solution2.java
@@ -0,0 +1,25 @@
+package kstar.leetcode._0217;
+
+import java.util.Arrays;
+
+/**
+ * <h6>排序+查找相邻元素法</h6>
+ */
+public class Solution2 {
+
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+
+        Arrays.sort(nums);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
