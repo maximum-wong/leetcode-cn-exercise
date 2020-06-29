361e41095a247ee5a8118b73939bd1cc9041a6e8
maximum2012 <maximum2012@qq.com>
2020-06-29 15:18:53 +0800

    Added 2 solutions for leetcode#215

---
+++ src/main/java/kstar/leetcode_cn/leetcode/_215/Solution.java
@@ -0,0 +1,49 @@
+package kstar.leetcode_cn.leetcode._215;
+
+import java.util.Random;
+
+/**
+ * <h6>快速排序法</h6>
+ */
+public class Solution {
+
+    private Random random = new Random();
+
+    public int findKthLargest(int[] nums, int k) {
+        return quickSelect(nums, 0, nums.length - 1, nums.length - k);
+    }
+
+    private int quickSelect(int[] a, int l, int r, int index) {
+        int q = randomPartition(a, l, r);
+        if (q == index) {
+            return a[q];
+        } else {
+            return q < index ? quickSelect(a, q + 1, r, index) : quickSelect(a, l, q - 1, index);
+        }
+    }
+
+    private int randomPartition(int[] a, int l, int r) {
+        int i = random.nextInt(r - l + 1) + l;
+        swap(a, i, r);
+        return partition(a, l, r);
+    }
+
+    private int partition(int[] a, int l, int r) {
+        int x = a[r];
+        int i = l - 1;
+        for (int j = l; j < r; j++) {
+            if (a[j] <= x) {
+                swap(a, ++i, j);
+            }
+        }
+        swap(a, i + 1, r);
+        return i + 1;
+    }
+
+    private void swap(int[] a, int i, int j) {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+}
---
+++ src/main/java/kstar/leetcode_cn/leetcode/_215/Solution2.java
@@ -0,0 +1,49 @@
+package kstar.leetcode_cn.leetcode._215;
+
+/**
+ * <h6>堆排序</h6>
+ */
+public class Solution2 {
+
+    public int findKthLargest(int[] nums, int k) {
+        int heapSize = nums.length;
+        buildMaxHeap(nums, heapSize);
+        for (int i = nums.length - 1; i >= nums.length - k + 1; --i) {
+            swap(nums, 0, i);
+            --heapSize;
+            maxHeapify(nums, 0, heapSize);
+        }
+        return nums[0];
+    }
+
+    private void buildMaxHeap(int[] a, int heapSize) {
+        for (int i = heapSize / 2; i >= 0; --i) {
+            maxHeapify(a, i, heapSize);
+        }
+    }
+
+    private void maxHeapify(int[] a, int i, int heapSize) {
+        int l = i * 2 + 1;
+        int r = i * 2 + 2;
+        int largest = i;
+
+        if (l < heapSize && a[l] > a[largest]) {
+            largest = l;
+        }
+
+        if (r < heapSize && a[r] > a[largest]) {
+            largest = r;
+        }
+
+        if (largest != i) {
+            swap(a, i, largest);
+            maxHeapify(a, largest, heapSize);
+        }
+    }
+
+    private void swap(int[] a, int i, int j) {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+}
