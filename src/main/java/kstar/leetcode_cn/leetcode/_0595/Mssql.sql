eb2017df242f75fe1423201049dfa31c959f015a
maximum-wong <maximum2012@qq.com>
2020-07-02 21:55:23 +0800

    Added a new solution for leetcode#595

---
+++ src/main/java/kstar/leetcode_cn/leetcode/_0595/Mssql.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+-- solution 1
+SELECT name, population, area FROM World WHERE area>3000000 OR population> 25000000;
+
+-- solution 2
+SELECT name, population, area FROM World WHERE area>3000000
+UNION
+SELECT name, population, area FROM World WHERE population> 25000000;
