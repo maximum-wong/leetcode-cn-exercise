da3e4ee00d91f66d9a69556fa4565dbd0e9a60b6
maximum-wong <maximum2012@qq.com>
2020-07-05 02:32:02 +0800

    Added solutions for leetcode#183

---
+++ src/main/java/kstar/leetcode_cn/leetcode/_0183/Mssql.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+-- solution 1
+SELECT c.Name AS Customers FROM Customers c LEFT JOIN Orders o ON c.Id=o.CustomerId WHERE o.CustomerId IS NULL;
+
+-- solution 2
+SELECT c.name AS Customers FROM Customers c WHERE c.id NOT IN (SELECT CustomerId FROM Orders);
+
+-- solution 3
+SELECT NAME AS Customers FROM Customers a WHERE NOT EXISTS (SELECT Id, CustomerId FROM Orders b WHERE a.id=b.CustomerId);
