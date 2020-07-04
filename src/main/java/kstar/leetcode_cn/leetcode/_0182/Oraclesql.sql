dc40bfdd59a07dfd3827dadb9d3430f6f1cd94fc
maximum-wong <maximum2012@qq.com>
2020-07-04 23:03:41 +0800

    Added solutions for leetcode#182

---
+++ src/main/java/kstar/leetcode_cn/leetcode/_0182/Oraclesql.sql
@@ -0,0 +1,9 @@
+/* Write your PL/SQL query statement below */
+-- solution 1
+SELECT Email FROM Person GROUP BY Email HAVING COUNT(Email) > 1
+
+-- solution 2
+SELECT Email FROM (SELECT Email, COUNT(Email) AS num FROM Person GROUP BY Email) tmp WHERE num>1
+
+-- solution 3
+SELECT DISTINCT a.email AS Email FROM person a INNER JOIN person b ON (a.email = b.email AND a.id <> b.id)
