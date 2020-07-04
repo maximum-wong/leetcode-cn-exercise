e651fc7f01fd79601ec82602c64e11117bbc478a
maximum-wong <maximum2012@qq.com>
2020-07-04 16:34:28 +0800

    Fixed bugs for leetcode#1179

--- src/main/java/kstar/leetcode_cn/leetcode/_0181/Mssql.sql
+++ src/main/java/kstar/leetcode_cn/leetcode/_0181/Mssql.sql
@@ -1,6 +1,6 @@
 /* Write your T-SQL query statement below */
 -- solution 1
-SELECT e1.Name FROM Employee e1, Employee e2 WHERE e1.ManagerId=e2.Id AND e1.Salary>e2.Salary;
+SELECT e1.Name AS Employee FROM Employee e1, Employee e2 WHERE e1.ManagerId=e2.Id AND e1.Salary>e2.Salary;
 
 -- solution 2
 SELECT a.Name AS Employee FROM Employee AS a LEFT JOIN Employee AS b ON a.ManagerId = b.id WHERE a.ManagerId IS NOT NULL AND a.Salary >b.Salary;
