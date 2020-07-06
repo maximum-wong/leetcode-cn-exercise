11b8736cc10556324c7abac9df1cc36612194629
maximum2012 <maximum2012@qq.com>
2020-07-06 09:33:53 +0800

    Added a new solution for leetcode#168

---
+++ src/main/java/kstar/leetcode_cn/leetcode/_0168/Solution.java
@@ -0,0 +1,14 @@
+package kstar.leetcode_cn.leetcode._0168;
+
+public class Solution {
+
+    public String convertToTitle(int n) {
+        StringBuilder sb = new StringBuilder();
+        while (n != 0) {
+            n -= 1;
+            sb.append((char) (n % 26 + 'A'));
+            n /= 26;
+        }
+        return sb.reverse().toString();
+    }
+}
