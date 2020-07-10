c719245e30480999f42c5cf9feb9851b1498fa62
maximum-wong <maximum2012@qq.com>
2020-07-11 02:57:21 +0800

    Added a solution for leetcode#1281

---
+++ src/main/java/kstar/leetcode_cn/leetcode/_1281/Solution.java
@@ -0,0 +1,17 @@
+package kstar.leetcode_cn.leetcode._1281;
+
+public class Solution {
+
+    public int subtractProductAndSum(int n) {
+        int sum = 0;
+        int product = 1;
+        int digit = 0;
+        while (n != 0) {
+            digit = n % 10;
+            sum += digit;
+            product *= digit;
+            n /= 10;
+        }
+        return product - sum;
+    }
+}
