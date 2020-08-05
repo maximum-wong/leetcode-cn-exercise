16629ca011457348b301add35efeed5ae3bff633
maximum2012 <maximum2012@qq.com>
2020-08-05 13:48:57 +0800

    Added a new solution for leetcode#434

---
+++ src/main/java/kstar/leetcode_cn/leetcode/_0434/Solution.java
@@ -0,0 +1,16 @@
+package kstar.leetcode_cn.leetcode._0434;
+
+public class Solution {
+
+    public int countSegments(String s) {
+        int segmentCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if ((i == 0 || s.charAt(i - 1) == ' ') && s.charAt(i) != ' ') {
+                segmentCount++;
+            }
+        }
+
+        return segmentCount;
+    }
+}
